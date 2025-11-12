bba95ab39b6dddb157120193c7c2fa5b28eb9ec6
vic-th <victorthaylor2@gmail.com>
2025-11-12 19:25:05 -0300

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+function Header() {
+return(
+    <header>
+<img src="" alt="" />
+
+<ul>
+    <li>LOOKS</li>
+    <li>LANÇAMENTOS</li>
+    <li>NOVIDADES</li>
+</ul>
+    </header>
+);
+}
+
+export default Header;
